df0fa69b9bc08e9c0fa62d24f572b3e9f03b9d71
bego <begomontes7@gmail.com>
2019-03-05 21:35:03 -0600

    Finished building restful apis with express

--- app.js
+++ app.js
@@ -17,28 +17,25 @@ app.get('/', (req,res)=>{
 });
 
 app.get('/api/courses', (req,res)=>{
-    res.send([1,2,3,4,5]);
+    res.send(courses);
 });
 
 app.get('/api/posts/:month/:year', (req,res)=>{
     res.send(req.params);
 });
-// app.get('/api/courses/:id',(req,res)=>{
-//     // find method available, argumnet expected a function, used to find something that matches a given criteria
-//     const course =courses.find(c => c.id === parseInt(req.params.id))
-//     if(!course) res.status(404).send("Course not found");
-//     res.send(course);
-// });
+app.get('/api/courses/:id',(req,res)=>{
+    // find method available, argumnet expected a function, used to find something that matches a given criteria
+    const course =courses.find(c => c.id === parseInt(req.params.id))
+    if(!course) return res.status(404).send("Course not found");
+    res.send(course);
+});
 
 app.post('/api/courses',(req,res)=>{
-    const schema ={
-        name:Joi.string().min(3).required()
-    };
-    const result =Joi.validate(req.body,schema);
-    console.log(result);
-    if(!req.body.coursename || req.body.coursename<3){
-       res.status(400).send('Coursename irs requiered and should be at least 3 characters') ;
-       return;
+    const {error}=validateCourse(req.body);
+    //validate
+    if(error){
+        return res.status(400).send(error.details[0].message);
+        
     }
     const course ={
         
@@ -50,6 +47,50 @@ app.post('/api/courses',(req,res)=>{
     courses.push(course);
     res.send(course);
 });
+
+app.put('/api/courses/:id',(req,res)=>{
+
+    // if not found send 404 error
+    const course =courses.find(c => c.id === parseInt(req.params.id))
+    if(!course){
+        return res.status(404).send("Course not found");
+       
+    } 
+    // {error} = result.error
+    // object destructuring
+    const {error}=validateCourse(req.body);
+    //validate
+    if(error){
+        return res.status(400).send(error.details[0].message);  
+    }
+
+    // update course
+    course.coursename=req.body.coursename;
+     // return the updatedcourse
+    res.send(course);
+   
+});
+
+function validateCourse(course){
+    const schema={
+        coursename: Joi.string().min(3).required()
+    };
+    return Joi.validate(course,schema);
+}
+
+app.delete('/api/courses/:id',(req,res)=>{
+    // Look up the course
+    const course =courses.find(c => c.id === parseInt(req.params.id))
+    // Not existing,return 404
+    if(!course) 
+    return res.status(404).send("Course not found");
+
+    // Delete
+    const index = courses.indexOf(course);
+    courses.splice(index,1);
+    // Return the same course
+    res.send(course);
+})
 
 const port = process.env.PORT || 3000;
 
